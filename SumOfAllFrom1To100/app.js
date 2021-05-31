eb9356009b1e45bc858c3071e1a7548684cdee8e
noreen <noreenakhtar922@gmail.com>
2021-05-31 20:34:48 +0500

    product of 1 to 100

---
+++ SumOfAllFrom1To100/app.js
@@ -0,0 +1,12 @@
+// program to display the sum of natural numbers
+
+// take input from the user
+const number = 100;
+
+let sum = 0;
+
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of all numbers:', sum);
