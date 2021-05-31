6b61f54ce8f012da5df9235f82db0d5b0e147228
noreen <noreenakhtar922@gmail.com>
2021-05-31 20:00:50 +0500

    using for loop console table in reverse

---
+++ ReverseTableOf5/app.js
@@ -0,0 +1,12 @@
+let n = 5;
+for (let i = 10; i >= 1; --i)
+    console.log( n + " * " +i +
+            " = " + n *
+                i +"<br>");
+
+
+
+                // var input  = prompt("Enter the number:");
+                // for (var i = 10; i >= 1; i--) {
+                // console.log(input + " * " + i + " = " + input * i);
+                // }
